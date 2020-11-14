817182bd6bd6966c9333acdbe84317b6ad2d4bc6
Roman <melnikov.roman.lang@gmail.com>
2020-11-15 01:49:20 +0600

    structure_2

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import s from './Profile.module.css';
+import MyPosts from './Posts/MyPosts.jsx'
 
 const Profile = () => {
     return (
         <main className={s.content}>
             <img src="https://cameronscookware.com/wp-content/uploads/2019/12/vaporwave-background-1920x1080-unique-vaporwave-minimalism-forest-1080p-laptop-full-hd-ideas-of-vaporwave-background-1920x1080-1.jpg" />
-            Main content
+            <MyPosts />
         </main>
     );
 }
