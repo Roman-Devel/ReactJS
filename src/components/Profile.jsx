68186bb756aa6ae713836c3ffcfc1673307f9253
Roman <melnikov.roman.lang@gmail.com>
2020-11-14 21:03:12 +0600

    first look

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <main className="content">
+            <img src="https://cameronscookware.com/wp-content/uploads/2019/12/vaporwave-background-1920x1080-unique-vaporwave-minimalism-forest-1080p-laptop-full-hd-ideas-of-vaporwave-background-1920x1080-1.jpg" />
+            Main content
+        </main>
+    );
+}
+
+export default Profile;
