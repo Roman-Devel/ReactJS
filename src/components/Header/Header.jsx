6d234c0758835f325463d4ea0c3351a656da9543
Roman <melnikov.roman.lang@gmail.com>
2020-11-14 23:28:31 +0600

    create css modules and structure project  modified

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className="header">
+        <header className={s.header}>
             <img src="https://upload.wikimedia.org/wikipedia/commons/1/1e/RPC-JP_Logo.png" />
         </header>
     );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <main className="content">
+        <main className={s.content}>
             <img src="https://cameronscookware.com/wp-content/uploads/2019/12/vaporwave-background-1920x1080-unique-vaporwave-minimalism-forest-1080p-laptop-full-hd-ideas-of-vaporwave-background-1920x1080-1.jpg" />
             Main content
         </main>
