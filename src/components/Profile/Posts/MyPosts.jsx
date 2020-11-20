2af8f4a4a9ee0724aa0acb73470c980fe6ac6528
Roman <melnikov.roman.lang@gmail.com>
2020-11-21 00:51:43 +0600

    props

---
+++ src/components/Profile/Posts/MyPosts.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './MyPosts.module.css';
+import MyPost from './Post/MyPost';
+
+const MyPosts = () => {
+    return (
+        <div className={s.posts}>
+            <MyPost message='Hi! How are you?' countLikes='17'/>
+            <MyPost message="It's my first post" countLikes='10'/>
+        </div>
+    );
+}
+
+export default MyPosts;
---
+++ src/components/Profile/Posts/Post/MyPost.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import s from './MyPost.module.css';
+
+const MyPost = (props) => {
+    return (
+        <div className={s.item}>
+            <div className={s.item__img}>
+                <img src='https://e7.pngegg.com/pngimages/999/998/png-clipart-graphics-avatar-mobile-app-website-user-staff-member-head-cartoon.png' />
+            </div>
+            {props.message}
+            <div>...Like: {props.countLikes}</div>
+        </div>
+    );
+}
+
+export default MyPost;
