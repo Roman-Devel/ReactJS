7492a64fc8871c1eddd76c2a0218b3c3748b5098
Roman <melnikov.roman.lang@gmail.com>
2020-11-24 01:11:29 +0600

    Router module

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';					/* Объект Router для переключения элементов */
+
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
 const App = () => {
 	return (
-		<div className='app-wrapper'>
-			<Header />
-			<Navbar />
-			<Profile />
-		</div>
+		<BrowserRouter>													{/* Обромляющий тег */}
+			<div className='app-wrapper'>
+				<Header />
+				<Navbar />
+				<div className='app-wrapper-content'>
+					<Route path='/dialogs' component={Dialogs} />		{/* Нужный маршрут, path-адресная строка и нужный компонент */}
+					<Route path='/profile' component={Profile} />
+					<Route path='/news' component={News} />
+					<Route path='/music' component={Music} />
+					<Route path='/settings' component={Settings} />
+				</div>
+			</div>
+		</BrowserRouter>
 	);
 }
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './Posts/MyPosts.jsx'
 
 const Profile = () => {
     return (
-        <main className={s.content}>
+        <main>
             <img src="https://cameronscookware.com/wp-content/uploads/2019/12/vaporwave-background-1920x1080-unique-vaporwave-minimalism-forest-1080p-laptop-full-hd-ideas-of-vaporwave-background-1920x1080-1.jpg" />
             <MyPosts />
         </main>
