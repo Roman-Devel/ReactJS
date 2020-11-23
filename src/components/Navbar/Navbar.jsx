2fac64261647aeb30b7b2112751ddaaa1287aa54
Roman <melnikov.roman.lang@gmail.com>
2020-11-24 01:49:57 +0600

    NavLink without a

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css';                      /* импортируем объект со стилями */
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
             <ul>
-                <li className={s.item}><a href="/profile">Profile</a></li>
-                <li className={s.item}><a href="/dialogs">Messages</a></li>
-                <li className={s.item}><a href="/news">News</a></li>
-                <li className={s.item}><a href="/music">Music</a></li>
-                <li className={s.item}><a href="/settings">Settings</a></li>
+                <li className={s.item}>
+                    <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>            {/* Замена обычной ссылки на основе Histori API без перезагрузки */}
+                </li>
+                <li className={s.item}>
+                    <NavLink to="/dialogs" activeClassName={s.active}>Messages</NavLink>           {/* Атрибут активного класса через модульный css */}
+                </li>
+                <li className={s.item}>
+                    <NavLink to="/news" activeClassName={s.active}>News</NavLink>
+                </li>
+                <li className={s.item}>
+                    <NavLink to="/music" activeClassName={s.active}>Music</NavLink>
+                </li>
+                <li className={s.item}>
+                    <NavLink to="/settings" activeClassName={s.active}>Settings</NavLink>
+                </li>
             </ul>
         </nav>
     );
